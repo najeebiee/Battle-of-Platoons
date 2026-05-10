db24cce4953fcb1b5233904e3bcf6e7882f589be
najeebiee <nmapantas_240000002090@uic.edu.ph>
2026-05-11 02:06:04 +0800

    new anon key

--- admin-app/src/services/supabase.js
+++ admin-app/src/services/supabase.js
@@ -2,7 +2,7 @@ import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
   "https://tkqkamywlsjdkpfeljrg.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRrcWthbXl3bHNqZGtwZmVsanJnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU4MjU5ODEsImV4cCI6MjA4MTQwMTk4MX0.1Qiq6RlwA6FJ7VkcIh9cuFKlye-WxvD1hju1eCcnxsk",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5qdm93ZXdha2ZvZndhbWN1cmdzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgyMzMyMzAsImV4cCI6MjA5MzgwOTIzMH0.Cgspn4pP7LcA04W1OoAxQ-CuPYSjAykxCdMY3qhNMV4",
   {
     auth: {
       persistSession: true,
